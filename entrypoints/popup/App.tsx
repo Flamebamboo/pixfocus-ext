dd2bb03a23e489fd72bafa4905a57c0cb81a78f3
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2025-02-23 14:06:39 +0800

    init

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
+import style from "/style.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
